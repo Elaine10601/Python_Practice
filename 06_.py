eb390c58e35d46c655e4309793b399ce7231b4fe
Elaine10601 <93778764+Elaine10601@users.noreply.github.com>
2021-11-06 21:43:40 +0800

    Update 06_.py

--- 06_.py
+++ 06_.py
@@ -1,3 +1,11 @@
+a=1
+while True:
+    a=a+1
+    print(a)
+    if a>100:
+        break
+
+
 #a陣列=[0,1,2,3,4]
 a=[0,1,2,3,4]
 #a陣列第0個到第2個(0到1)=[3,4,5]
@@ -14,3 +22,8 @@ a[0:2]=[7,4,0,6,8]
 print(a)
 
 
+a=[1,2,3,5]
+print(4 not in a)
+print(4 in a)
+c=int(input('2'))
+print(c in a)
